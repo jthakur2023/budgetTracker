47578e6ad011e3fbab4471c33b0817fae1c7c70c
willwat <willwatson220@gmail.com>
2020-06-28 18:34:14 -0400

    Linked budgets and add budget screens

--- app/src/main/java/org/uc/budgettracker/ui/AddBudget.kt
+++ app/src/main/java/org/uc/budgettracker/ui/AddBudget.kt
@@ -1,32 +1,29 @@
 package org.uc.budgettracker.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.uc.budgettracker.R
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class BudgetScreen : Fragment() {
+class AddBudget : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.budget_screen, container, false)
+        return inflater.inflate(R.layout.add_budget, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.btnAddBudget).setOnClickListener {
-            findNavController().navigate(R.id.action_BudgetScreen_to_AddBudget)
+        view.findViewById<Button>(R.id.btnDone).setOnClickListener {
+            findNavController().navigate(R.id.action_AddBudget_Done)
         }
     }
 }
