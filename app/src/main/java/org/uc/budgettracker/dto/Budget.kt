add659a88091ccb43f6775b4890755587ed7fba8
bradyk4 <bradyk4@mail.uc.edu>
2020-07-05 22:11:55 -0400

    Code Review Changes

--- app/src/main/java/org/uc/budgettracker/dto/Budget.kt
+++ app/src/main/java/org/uc/budgettracker/dto/Budget.kt
@@ -3,6 +3,7 @@ package org.uc.budgettracker.dto
 data class Budget(var name: String, var budgetAmt: Double, var incomeAmt: Double, var interval: TimeInterval) {
     enum class TimeInterval {
         // DAILY, WEEKLY, MONTHLY, YEARLY
+        //commented out this line until the dto is utilized
     }
 
     //function not used
